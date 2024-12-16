e18f0a6b8ed8845aa2e80f4a41dfb2091e599e35
Nicolás Torres <torresnicolas@gmail.com>
2024-12-16 15:12:41 +0000

    chore: update configurations, add Firebase setup, and ESLint config

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebase:{
+        "projectId": "xxxxxxxxxxxxxxxxxxxxxxxxx",
+        "appId": "x:xxxxxxxxxxx:web:xxxxxxxxxxxxxxxxxxxx",
+        "storageBucket": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+        "apiKey": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+        "authDomain": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+        "messagingSenderId": "xxxxxxxxxxx"
+    }
+};
