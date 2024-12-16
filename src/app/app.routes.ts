cda306917ef0426647addea49b0763591e15fc78
Nicolás Torres <torresnicolas@gmail.com>
2024-12-16 16:16:30 +0000

    feat(auth): add authentication buttons in toolbar and app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,18 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+import { privateGuard, publicGuard } from "./core/auth.guard";
 
 export const routes: Routes = [
-    { path: "", redirectTo: "/home", pathMatch: "full" },
-    {
-        path: "home",
-        loadComponent: () =>
-          import("./features/home/home.component").then((m) => m.HomeComponent),
-    },    
-    { path: "**", redirectTo: "/home" },
+  { path: "", redirectTo: "/home", pathMatch: "full" },
+  {
+    canActivateChild: [publicGuard()],
+    path: "auth",
+    loadChildren: () => import("./auth/features/auth.routes"),
+  },
+  {
+    canActivateChild: [publicGuard()],
+    path: "home",
+    loadComponent: () =>
+      import("./features/home/home.component").then((m) => m.HomeComponent),
+  },
+  { path: "**", redirectTo: "/home" },
 ];
--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -3,19 +3,24 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { Router, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { APP_CONSTANTS } from '@shared/constants';
+import { AuthStateService } from '@shared/data-access/auth-state.service';
 
-const MATERIAL_MODULES = [MatIconModule, MatButtonModule, MatToolbarModule, RouterModule];
+const MATERIAL_MODULES = [MatIconModule, MatButtonModule, MatToolbarModule, RouterModule, CommonModule];
 
 @Component({
   selector: 'app-toolbar',
   standalone: true,
   imports: [MATERIAL_MODULES],
-  templateUrl: './toolbar.component.html'
+  templateUrl: './toolbar.component.html',
+  styleUrl: './toolbar.component.scss'
 })
 export class ToolbarComponent implements OnInit {
   private router = inject(Router);
+  private authStateService = inject(AuthStateService);
   currentRoute = '';
+  isAuthenticated = false;
 
   get appConstants() {
     return APP_CONSTANTS;
@@ -25,6 +30,17 @@ export class ToolbarComponent implements OnInit {
     this.router.events.subscribe(() => {
       this.currentRoute = this.router.url;
     });
+    this.authStateService.authState$.subscribe(user => {
+      this.isAuthenticated = !!user;
+    });  
   }
 
+  async logOut(): Promise<void> {
+    try {
+      await this.authStateService.logOut();
+      this.router.navigate(['/login']);
+    } catch (error) {
+      console.error('Error al cerrar sesión:', error);
+    }
+  }
 }
